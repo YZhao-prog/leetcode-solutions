57a81b915478f8b713d15f229df1b160f92417df
Yang Zhao <yangzhao200124@gmail.com>
2026-01-23 18:23:48 -0500

    Time: 3 ms (93.71%), Space: 6.2 MB (49.9%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+    p, q := head, head
+    for q != nil && q.Next != nil {
+        p = p.Next
+        q = q.Next.Next
+        if p == q {
+            return true
+        }
+    }
+    return false
+}
