84fb8a6e579158904f9c75fd076b9cff3b7c6bd3
Yang Zhao <yangzhao200124@gmail.com>
2026-01-30 19:00:04 -0500

    Time: 0 ms (100%), Space: 4.5 MB (46.67%) - LeetHub

---
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.go
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func flipEquiv(root1 *TreeNode, root2 *TreeNode) bool {
+	if root1 == nil || root2 == nil {
+		return root1 == root2
+	}
+	return root1.Val == root2.Val && (flipEquiv(root1.Left, root2.Left) && (flipEquiv(root1.Right, root2.Right)) || (flipEquiv(root1.Right, root2.Left) && flipEquiv(root1.Left, root2.Right)))
+}
