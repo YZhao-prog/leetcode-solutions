555128bd137ccd5ebc40a7a60747a30e8421d8e3
Yang Zhao <yangzhao200124@gmail.com>
2026-02-20 19:11:25 -0500

    Time: 0 ms (100%), Space: 3.9 MB (94.43%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.go
@@ -0,0 +1,21 @@
+func letterCombinations(digits string) (ans []string) {
+    n := len(digits)
+    if n == 0 {
+        return nil
+    }
+    var mapping = [...]string{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
+    path := make([]byte, n)
+    var dfs func(int)
+    dfs = func(i int) {
+        if i == n {
+            ans = append(ans, string(path))
+            return
+        }
+        for _, c := range mapping[digits[i] - '0'] {
+            path[i] = byte(c)
+            dfs(i + 1)
+        }
+    }
+    dfs(0)
+    return ans
+}
